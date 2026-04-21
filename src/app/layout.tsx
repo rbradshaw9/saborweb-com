ffbe13ac541b223df0c691020248a16d7b05a01e
Ryan Bradshaw <rbradshaw@gmail.com>
2026-04-21 15:24:36 -0400

    fix: load tag manager before hydration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="es" translate="no" className={`${inter.variable} ${spaceGrotesk.variable}`}>
+      {analyticsEnabled && (
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${gtmId}');
+          `}
+        </Script>
+      )}
       <body>
         {analyticsEnabled && (
           <noscript>
@@ -55,17 +66,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {children}
         {analyticsEnabled && <AnalyticsPageView />}
       </body>
-      {analyticsEnabled && (
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${gtmId}');
-          `}
-        </Script>
-      )}
     </html>
   );
 }
