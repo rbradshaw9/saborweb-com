40f9f3f0353a5ed858c61476031ecad16dded011
Ryan Bradshaw <rbradshaw@gmail.com>
2026-04-27 14:15:47 -0400

    Force admin routes to render dynamically

--- src/app/(admin)/admin/(protected)/layout.tsx
+++ src/app/(admin)/admin/(protected)/layout.tsx
@@ -2,6 +2,8 @@ import { requireAdminUser } from '@/lib/admin/auth';
 import { AdminLiveSync } from './AdminLiveSync';
 import './admin.css';
 
+export const dynamic = 'force-dynamic';
+
 export default async function AdminProtectedLayout({ children }: { children: React.ReactNode }) {
   await requireAdminUser();
 
